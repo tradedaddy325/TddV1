e4bacf2880755bc308ce65d95c480bb2f78233e4
tradedaddy325 <mbhorat7861@gmail.com>
2026-04-07 12:33:23 +0200

    Update proxy.js
    fix deployment errors

--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
 import { updateSession } from '@/lib/supabase/proxy'
-import { type NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
