97b65c1bed4ea768c1eea6baa22f06b696203acf
tradedaddy325 <mbhorat7861@gmail.com>
2026-04-07 12:54:59 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { updateSession } from '@/lib/supabase/proxy'
-import { type NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request) {
   return await updateSession(request)
 }
 
