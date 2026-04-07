cf7f101a6fe168de6fa6a331d62dab05f783b34b
tradedaddy325 <mbhorat7861@gmail.com>
2026-04-07 12:38:38 +0200

    Update proxy.js
    fix deployment issue 2

--- proxy.js
+++ proxy.js
@@ -1,10 +1,9 @@
 import { updateSession } from '@/lib/supabase/proxy'
 import { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request) {
   return await updateSession(request)
 }
-
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
