f1a051bfc519c66f28d5971a195eb9aa20bbf8d0
tradedaddy325 <mbhorat7861@gmail.com>
2026-04-11 17:05:28 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,9 +1,10 @@
+import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/proxy'
-import { NextRequest } from 'next/server'
 
-export async function middleware(request) {
+export async function proxy(request: NextRequest) {
   return await updateSession(request)
 }
+
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
